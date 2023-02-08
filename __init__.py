ffe9b51238e918d82ac5e4a53e05d52b7cf00aba
Beatrice Apollo <beatrice.apollo.00@gmail.com>
2023-02-08 16:52:22 +0100

    prova1

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+import nltk
+from nltk.tokenize import word_tokenize
 
 
 class Nltkprova(MycroftSkill):
@@ -7,7 +9,8 @@ class Nltkprova(MycroftSkill):
 
     @intent_file_handler('nltkprova.intent')
     def handle_nltkprova(self, message):
-        self.speak_dialog('nltkprova')
+        py_txt = word_tokenize (message)
+        self.speak_dialog(py_txt)
 
 
 def create_skill():
